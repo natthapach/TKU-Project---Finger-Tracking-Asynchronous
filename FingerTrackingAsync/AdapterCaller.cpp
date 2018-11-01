c6d77fbab396fa467cdd6a73b28f008df172adf7
natthapach <natthapach.a@ku.th>
2018-11-01 20:48:51 +0800

    prepare to send data with socket

---
+++ FingerTrackingAsync/AdapterCaller.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "AdapterCaller.h"
+
+void AdapterCaller::testSend(int n)
+{
+	
+}
---
+++ FingerTrackingAsync/AdapterCaller.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <io.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <Winsock2.h>
+using namespace std;
+
+class AdapterCaller {
+public :
+	void testSend(int n);
+protected :
+	const string HOST = "127.0.0.1";
+	const int PORT = 8081;
+};
